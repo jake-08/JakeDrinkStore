bef962a3236ad032288a9c7f9cde131ef311c68c
JakeLin <jake.lin@dash.com.au>
2023-02-23 20:57:52 +1100

    Implement Increment and Decrement cart items in Shopping Cart page

--- JakeDrinkStoreWeb/Areas/Customer/Controllers/CartController.cs
+++ JakeDrinkStoreWeb/Areas/Customer/Controllers/CartController.cs
@@ -17,7 +17,8 @@ namespace JakeDrinkStoreWeb.Areas.Customer.Controllers
         {
             _unitOfWork = unitOfWork;
         }
-        public IActionResult Index()
+		#region Index Page
+		public IActionResult Index()
         {
 			// Get the login user id
 			var claimsIdentity = (ClaimsIdentity)User.Identity;
@@ -36,6 +37,82 @@ namespace JakeDrinkStoreWeb.Areas.Customer.Controllers
 
 			return View(ShoppingCartVM);
         }
+
+		public IActionResult IndividualPlus(int cartId)
+		{
+			var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(sc => sc.Id == cartId);
+			_unitOfWork.ShoppingCart.IncrementCount(cart, 1);
+			_unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+		public IActionResult IndividualMinus(int cartId)
+		{
+			var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(sc => sc.Id == cartId);
+			if (cart.Count <= 1)
+			{
+				_unitOfWork.ShoppingCart.Remove(cart);
+			}
+			else
+			{
+				_unitOfWork.ShoppingCart.DecrementCount(cart, 1);
+			}
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+		public IActionResult IndividualRemove(int cartId)
+		{
+			var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(sc => sc.Id == cartId);
+			if (cart.CaseCount >= 1)
+			{
+				_unitOfWork.ShoppingCart.DecrementCount(cart, cart.Count);
+			}
+			else
+			{
+				_unitOfWork.ShoppingCart.Remove(cart);
+			}
+			_unitOfWork.Save();
+			return RedirectToAction(nameof(Index));
+		}
+
+        public IActionResult CasePlus(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(sc => sc.Id == cartId);
+            _unitOfWork.ShoppingCart.IncrementCaseCount(cart, 1);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult CaseMinus(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(sc => sc.Id == cartId);
+            if (cart.CaseCount <= 1)
+            {
+                _unitOfWork.ShoppingCart.Remove(cart);
+            }
+            else
+            {
+                _unitOfWork.ShoppingCart.DecrementCaseCount(cart, 1);
+            }
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+		public IActionResult CaseRemove(int cartId)
+		{
+			var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(sc => sc.Id == cartId);
+			if (cart.Count >= 1)
+			{
+				_unitOfWork.ShoppingCart.DecrementCaseCount(cart, cart.CaseCount);
+			}
+			else
+			{
+				_unitOfWork.ShoppingCart.Remove(cart);
+			}
+			_unitOfWork.Save();
+			return RedirectToAction(nameof(Index));
+		}
 
 		private double GetIndividualPrice(int individualQuantity, double individualPrice)
 		{
@@ -60,5 +137,30 @@ namespace JakeDrinkStoreWeb.Areas.Customer.Controllers
 				return casePrice;
 			}
 		}
+		#endregion Index Page
+
+		#region Summary Page
+		public IActionResult Summary()
+		{
+			// Get the login user id
+			//	var claimsIdentity = (ClaimsIdentity)User.Identity;
+			//	var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+			//	ShoppingCartVM = new ShoppingCartVM()
+			//	{
+			//		ListCart = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value, includeProperties: "Product"),
+			//	};
+
+			//	foreach (var cart in ShoppingCartVM.ListCart)
+			//	{
+			//		ShoppingCartVM.CartTotal += (GetIndividualPrice(cart.Count, cart.Product.ListPrice) * cart.Count) +
+			//									(GetCasePrice(cart.CaseCount, cart.Product.MinBulkCase, cart.Product.CasePrize, cart.Product.BulkCasePrice) * cart.CaseCount);
+			//	}
+
+			//	return View(ShoppingCartVM);
+			return View();
+		}
+		#endregion Summary Page
+
 	}
 }
